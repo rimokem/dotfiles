c19b7919e9f5f5a89a6026ae36db218203a467db
rimokem <115140645+rimokem@users.noreply.github.com>
2026-04-02 16:29:40 +0900

    Add .config/nvim/LICENSE Add .config/nvim/README.md Add .config/nvim/.gitignore Add .config/nvim/.neoconf.json Add .config/nvim/init.lua Add .config/nvim/lazy-lock.json Add .config/nvim/lazyvim.json Add .config/nvim/lua/config/autocmds.lua Add .config/nvim/lua/config/keymaps.lua Add .config/nvim/lua/config/lazy.lua Add .config/nvim/lua/config/options.lua Add .config/nvim/lua/plugins/example.lua Add .config/nvim/lua/plugins/lsp.lua Add .config/nvim/lua/plugins/obsidian.lua Add .config/nvim/stylua.toml

---
+++ dot_config/nvim/lua/plugins/lsp.lua
@@ -0,0 +1,12 @@
+return {
+  "neovim/nvim-lspconfig",
+  opts = {
+    servers = {
+      -- Ruff (Linter & Formatter / Language Server)
+      ruff = {},
+
+      -- ty (Type Checker & Language Server)
+      ty = {},
+    },
+  },
+}
